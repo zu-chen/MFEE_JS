173c0c54af36d9d31de977e8f793ea9c0b1cc7a7
Zu <a2468929100@gmail.com>
2020-12-06 23:02:29 +0800

    master

--- 1125/test/05.draw_card.js
+++ 1125/test/05.draw_card.js
@@ -1,6 +1,27 @@
 const display = document.getElementById('display')
-const btn = document.getElementById('btn')
+const start = document.getElementById('start')
+const reset = document.getElementById('reset')
 
-btn.addEventListener('click',function(){
-  
+start.addEventListener('click', function () {
+  for (var j = 1; j < 10; j++) {
+    var i = Math.floor(Math.random() * 100) + 1
+    // 1-3
+    if (i < 4) {
+      display.innerHTML += '<span style="color:gold">五星卡</span>'
+    }
+
+    // 4-30
+    if ((i > 3) & (i < 31)) {
+      display.innerHTML += '<span style="color:silver">四星卡</span>'
+    }
+
+    // 31-100
+    if (i > 30) {
+      display.innerHTML += '三星卡'
+    }
+  }
+})
+
+reset.addEventListener('click', function () {
+  display.innerHTML = ''
 })
