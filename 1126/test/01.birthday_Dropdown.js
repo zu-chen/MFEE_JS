c5063a122d5b6b7b71a9d170095fdd6fab4ee212
Zu <a2468929100@gmail.com>
2020-12-09 20:54:33 +0800

    master

--- 1126/test/01.birthday_Dropdown.js
+++ 1126/test/01.birthday_Dropdown.js
@@ -2,3 +2,21 @@
 const year = document.getElementById('year')
 const month = document.getElementById('month')
 const date = document.getElementById('date')
+
+let yearString = `<option value="">請選擇西元年</option>`
+for (i = 1910; i < 2011; i++) {
+  yearString += `<option value="">${i}</option>`
+}
+year.innerHTML = yearString
+
+let monthString = `<option value="">請選擇月份</option>`
+for (i = 1; i < 13; i++) {
+  monthString += `<option value="">${i}</option>`
+}
+month.innerHTML = monthString
+
+let dateString = `<option value="">請選擇日期</option>`
+for (i = 1; i < 32; i++) {
+  dateString += `<option value="">${i}</option>`
+}
+date.innerHTML = dateString
