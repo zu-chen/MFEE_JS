2f6e1065fb90d8748171e6dbd8b52be306c782ca
Zu <a2468929100@gmail.com>
2020-12-24 14:15:58 +0800

    1126

--- 1126/test/01.birthday_Dropdown.js
+++ 1126/test/01.birthday_Dropdown.js
@@ -3,32 +3,32 @@ const year = document.getElementById('year')
 const month = document.getElementById('month')
 const date = document.getElementById('date')
 
-let yearString = `<option value="">請選擇西元年</option>`
-for (i = 1910; i < 2011; i++) {
-  yearString += `<option value="">${i}</option>`
+let yearString = `<option value="0">請選擇西元年</option>`
+for (let i = 1910; i < 2011; i++) {
+  yearString += `<option value="${i}">${i}</option>`
 }
 year.innerHTML = yearString
 
-let monthString = `<option value="">請選擇月份</option>`
-for (i = 1; i < 13; i++) {
-  monthString += `<option value="">${i}</option>`
+let monthString = `<option value="0">請選擇月份</option>`
+for (let i = 1; i < 13; i++) {
+  monthString += `<option value="${i}">${i}</option>`
 }
 month.innerHTML = monthString
 
-let dateString = `<option value="">請選擇日期</option>`
-for (i = 1; i < 32; i++) {
-  dateString += `<option value="">${i}</option>`
+let dateString = `<option value="0">請選擇日期</option>`
+for (let i = 1; i < 32; i++) {
+  dateString += `<option value="${i}">${i}</option>`
 }
 date.innerHTML = dateString
 
-year.addEventListener('change', function () {
-  month.innerHTML = `<option value="">請選擇月份</option>`
-  date.innerHTML = `<option value="">請選擇日期</option>`
-})
+// year.addEventListener('change', function () {
+//   month.innerHTML = `<option value="">請選擇月份</option>`
+//   date.innerHTML = `<option value="">請選擇日期</option>`
+// })
 
-month.addEventListener('change', function () {})
+// month.addEventListener('change', function () {})
 
 // addEventListener未做
 
 // 顯示是否滿18歲
-if (year.value)
+// if (year.value)
