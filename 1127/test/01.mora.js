8322d60c0421058ba2cb4903ff3a36d3b56d08db
Zu <a2468929100@gmail.com>
2020-12-24 20:51:39 +0800

    剪刀石頭布頁面

---
+++ 1127/test/01.mora.js
@@ -0,0 +1,7 @@
+// 從"3.生日下拉清單-完成"開始
+const scissors = document.getElementById('scissors')
+const ston = document.getElementById('ston')
+const paper = document.getElementById('paper')
+const user = document.getElementById('user')
+const computer = document.getElementById('computer')
+const result = document.getElementById('result')
